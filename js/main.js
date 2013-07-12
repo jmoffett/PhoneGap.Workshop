af58d654ec876adf310ea63e6873444a743e9c0d
John Moffett <john.moffett@gmail.com>
2013-07-12 10:46:06 -0500

    Part 3: Using Native Notification

--- js/main.js
+++ js/main.js
@@ -13,9 +13,20 @@ var app = {
         });
     },
 
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
+
     initialize: function () {
+        var self = this;
         //this.store = new MemoryStore();
-        this.store = new LocalStorageStore();
+        this.store = new LocalStorageStore(function () {
+            self.showAlert('Store Initialized', 'Info');
+        });
         //this.store = new WebSqlStore();
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
